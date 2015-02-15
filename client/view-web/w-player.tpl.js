cbc03c05e7569990b33b50102e39677bc223e3a3
Hiraash Thawfeek <hiraash@cinergix.com>
2015-02-15 14:26:29 +0530

    Decent looking player for the initial phase

--- client/view-web/w-player.tpl.js
+++ client/view-web/w-player.tpl.js
@@ -1,7 +1,32 @@
 Template.wPlayer.helpers({
 	
 	active: function () {
-		return (PlayList.find().count() > 0);	
+		// return (PlayList.find().count() > 0);	
+		return true;
 	}
 
 });
+
+Template.wPlayer.rendered = function () {
+	
+    $("#jp-control").jPlayer({
+        ready: function(event) {
+            $(this).jPlayer("setMedia", {
+				title: "http://localhost:3000/audio.mp3",
+				mp3: "http://localhost:3000/audio.mp3"
+            });
+
+        },
+        swfPath: "http://jplayer.org/latest/dist/jplayer",
+        supplied: "mp3, oga",
+		wmode: "window",
+		useStateClassSkin: true,
+		autoBlur: false,
+		smoothPlayBar: true,
+		keyEnabled: true,
+		remainingDuration: true,
+		toggleDuration: true
+    });
+
+
+};
