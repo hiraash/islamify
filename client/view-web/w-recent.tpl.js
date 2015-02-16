2b40806ded8bcf2eed1c82537617e01c9b65ad06
Hiraash Thawfeek <hiraash@cinergix.com>
2015-02-16 18:31:48 +0530

    Adding presentation to the lecture

--- client/view-web/w-recent.tpl.js
+++ client/view-web/w-recent.tpl.js
@@ -22,6 +22,24 @@ Template.wRecent.helpers({
 			duration: '1:12:26',
 			language: 'English',
 			tags:[ 'pride','ego','simplicity' ]
+		},{
+			name: 'Sadaqa Ja- Riyah Best Returns On Invesment',
+			speaker: 'Sheikh Mufti Ismail Menk',
+			duration: '36:42',
+			language: 'English',
+			tags:[ 'sadaqa','jummah','investment','dunya' ]
+		},{
+			name: 'Nikha and Family Life',
+			speaker: 'Mufti Yoosuff Haniffa',
+			duration: '2:12:01',
+			language: 'Tamil',
+			tags:[ 'nikha','family','marriage','wedding' ]
+		},{
+			name: 'Ego And Fame  - “Nose In the Air, Feet Off The Ground”',
+			speaker: 'Sheikh Mufti Ismail Menk',
+			duration: '1:12:26',
+			language: 'English',
+			tags:[ 'pride','ego','simplicity' ]
 		}]
 	}
 
